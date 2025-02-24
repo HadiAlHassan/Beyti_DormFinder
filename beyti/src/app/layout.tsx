ab263ccb5f6d2c4352cf931b40e46ac6d232d3c0
Hadi Al Hassan <hadialhassan90@gmail.com>
2025-02-25 01:19:15 +0200

    1 Added Shadcn Components 2 Adjusted some packages 3 changed global css folder

--- beyti/src/app/layout.tsx
+++ beyti/src/app/layout.tsx
@@ -1,4 +1,6 @@
 import type { Metadata } from "next";
+import { ThemeProvider } from "@/components/theme-provider"
+
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -23,12 +25,22 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
+      <head />
       <body
+      
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
       </body>
     </html>
   );
 }
+
